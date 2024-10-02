0fe46b45414f435db56fe221a042036ac2d30a49
clatthew <mattjamesclough@gmail.com>
2024-10-02 12:48:49 +0100

    add brands table (which i forgot)

--- models/03_models.sql
+++ models/03_models.sql
@@ -1,13 +1,13 @@
 \c topmodelsql;
 
-CREATE TABLE locations_3nf AS
+CREATE TABLE areas_3nf AS
 SELECT DISTINCT
     area
 FROM
     models_2nf;
 
-ALTER TABLE locations_3nf
-ADD location_id SERIAL PRIMARY KEY;
+ALTER TABLE areas_3nf
+ADD area_id SERIAL PRIMARY KEY;
 
 CREATE TABLE traits_3nf AS
 SELECT DISTINCT
@@ -42,12 +42,12 @@ CREATE TABLE models_3nf AS
 SELECT
     model_id
     , model_name
-    , location_id
+    , area_id
     , price_per_event
     , category_id
     , trait_id
 FROM models_2nf
-JOIN locations_3nf
+JOIN areas_3nf
     USING (area)
 JOIN categories_3nf
     USING (category)
@@ -57,8 +57,33 @@ JOIN traits_3nf
 ALTER TABLE models_3nf
 ADD PRIMARY KEY (model_id);
 
-SELECT * FROM locations_3nf;
+-- SELECT * FROM model_jobs_2nf;
+
+CREATE TABLE brands_3nf AS
+SELECT DISTINCT
+    brand as brand_name
+FROM model_jobs_2nf;
+
+ALTER TABLE brands_3nf
+ADD brand_id SERIAL PRIMARY KEY;
+
+CREATE TABLE brand_relationships_3nf AS
+SELECT DISTINCT
+    model_id
+    , brand_id
+FROM model_jobs_2nf
+JOIN brands_3nf
+    ON model_jobs_2nf.brand = brands_3nf.brand_name;
+
+ALTER TABLE brand_relationships_3nf
+ADD PRIMARY KEY (model_id, brand_id);
+
+SELECT * FROM areas_3nf;
 SELECT * FROM traits_3nf;
 SELECT * FROM categories_3nf;
 SELECT * FROM events_3nf;
 SELECT * FROM models_3nf;
+SELECT * FROM brands_3nf;
+SELECT * FROM brand_relationships_3nf;
+
+\d
